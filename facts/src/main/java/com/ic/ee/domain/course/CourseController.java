7f89f98c0198ac92c02f9da167845fc7bba83c81
Cian Leow <cianyleow@gmail.com>
2016-03-16 13:55:38 +0000

    Made controller available on /courses URL and moved to service design.

--- facts/src/main/java/com/ic/ee/domain/course/CourseController.java
+++ facts/src/main/java/com/ic/ee/domain/course/CourseController.java
@@ -6,27 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ic.ee.core.service.api.CourseService;
 
-@RestController(value = "/courses")
+@RestController
 public class CourseController {
 
 	@Autowired
 	private CourseService courseService;
 
-	@RequestMapping(path = "/", method = RequestMethod.GET)
+	@RequestMapping(path = "/courses", method = RequestMethod.GET)
 	public List<CourseDetails> getCourses() {
 		return courseService.getAllCourseDetails();
 	}
 
-	@RequestMapping(path = "/{courseId}", method = RequestMethod.GET)
+	@RequestMapping(path = "/courses/{courseId}", method = RequestMethod.GET)
 	public Course getCourse(@PathVariable("courseId") Integer courseId) {
 		return courseService.getCourse(courseId);
 	}
 
-	@RequestMapping(path = "/{courseId}/details", method = RequestMethod.GET)
+	@RequestMapping(path = "/courses/{courseId}/details", method = RequestMethod.GET)
+	@ResponseBody
 	public CourseDetails getCourseDetails(@PathVariable("courseId") Integer courseId) {
 		return courseService.getCourseDetails(courseId);
 	}
