62b2deb20f60ceaeb3c2c3e96434bdd138f0216b
Cian Leow <cianyleow@gmail.com>
2016-03-18 16:14:32 +0000

    Revert "Revert "Added AcademicPeriodService""
    This reverts commit 6bc41fb2a7e21a0d8105e8e47f1f3eed8fe2ee99.

---
+++ facts/src/main/java/com/ic/ee/core/service/api/AcademicPeriodService.java
@@ -0,0 +1,7 @@
+package com.ic.ee.core.service.api;
+
+import java.util.List;
+
+public interface AcademicPeriodService {
+	public List<Integer> getCourseIdsFromAcademicPeriod(Integer academicPeriodId);
+}
