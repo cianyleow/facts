e01067cdc5438627243118c6a602d8c54bdcd7d5
Cian Leow <cianyleow@gmail.com>
2016-03-30 11:56:45 +0100

    Wired in enrollment service and used methods.

--- facts/src/main/java/com/ic/ee/controller/EnrollmentController.java
+++ facts/src/main/java/com/ic/ee/controller/EnrollmentController.java
@@ -3,6 +3,7 @@ package com.ic.ee.controller;
 import java.security.Principal;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,37 +11,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ic.ee.domain.common.relationship.Enrollment;
+import com.ic.ee.service.api.EnrollmentService;
 
 @RestController
 public class EnrollmentController {
 
+	@Autowired
+	private EnrollmentService enrollmentService;
+
 	@RequestMapping("/enrollments/me")
 	public List<Enrollment> getEnrollments(Principal user) {
-		return null;
+		return enrollmentService.getEnrollments(user.getName());
 	}
 
 	@RequestMapping("/enrollments/{username}")
 	public List<Enrollment> getEnrollments(@PathVariable("username") String username) {
-		return null;
+		return enrollmentService.getEnrollments(username);
 	}
 
 	@RequestMapping(path = "/enrollments/{enrollmentId}/details", method = RequestMethod.GET)
 	public Enrollment getEnrollment(@PathVariable("enrollmentId") Integer enrollmentId) {
-		return null;
+		return enrollmentService.getEnrollment(enrollmentId);
 	}
 
 	@RequestMapping(path = "/enrollments/{enrollmentId}/details", method = RequestMethod.POST)
 	public Integer addEnrollment(@RequestBody Enrollment enrollment) {
-		return 0;
+		return enrollmentService.addEnrollment(enrollment);
 	}
 
 	@RequestMapping(path = "/enrollments/{enrollmentId}/details", method = RequestMethod.PUT)
-	public void updateEnrollment(@PathVariable("enrollmentId") Integer enrollmentId) {
-
+	public void updateEnrollment(@PathVariable("enrollmentId") Integer enrollmentId, @RequestBody Enrollment enrollment) {
+		enrollmentService.updateEnrollment(enrollmentId, enrollment);
 	}
 
 	@RequestMapping(path = "/enrollments/{enrollmentId}/details", method = RequestMethod.DELETE)
 	public void deleteEnrollment(@PathVariable("enrollmentId") Integer enrollmentId) {
-
+		enrollmentService.deleteEnrollment(enrollmentId);
 	}
 }
