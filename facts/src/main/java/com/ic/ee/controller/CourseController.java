536b55e258e95ed3842668a147adac55b59f157e
Cian Leow <cianyleow@gmail.com>
2016-03-30 15:38:14 +0100

    Added all GET methods to CourseController

--- facts/src/main/java/com/ic/ee/controller/CourseController.java
+++ facts/src/main/java/com/ic/ee/controller/CourseController.java
@@ -12,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ic.ee.core.web.exception.NoResultsReturnedException;
 import com.ic.ee.core.web.exception.TooManyResultsReturnedException;
 import com.ic.ee.domain.course.Course;
-import com.ic.ee.domain.course.assignment.AssignmentDetails;
+import com.ic.ee.domain.course.assignment.Assignment;
+import com.ic.ee.domain.course.assignment.submission.Submission;
+import com.ic.ee.domain.user.courseowner.CourseOwner;
+import com.ic.ee.domain.user.marker.Marker;
+import com.ic.ee.domain.user.student.Student;
 import com.ic.ee.service.api.AssignmentService;
 import com.ic.ee.service.api.CourseService;
 
@@ -25,15 +29,49 @@ public class CourseController {
 	@Autowired
 	private AssignmentService assignmentService;
 
+	@RequestMapping(path = "/courses", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Course> getCourses() {
+		return null;
+	}
+
 	@RequestMapping(path = "/courses/{courseId}", method = RequestMethod.GET)
 	@ResponseBody
 	public Course getCourse(@PathVariable("courseId") Integer courseId) throws NoResultsReturnedException, TooManyResultsReturnedException {
 		return courseService.getCourse(courseId);
 	}
 
+	@RequestMapping(path = "/courses/{courseId}/students", method = RequestMethod.GET)
+	public List<Student> getStudents(@PathVariable("courseId") Integer courseId) {
+		return null;
+	}
+
+	@RequestMapping(path = "/courses/{courseId}/markers", method = RequestMethod.GET)
+	public List<Marker> getMarkers(@PathVariable("courseId") Integer courseId) {
+		return null;
+	}
+
+	@RequestMapping(path = "/courses/{courseId}/courseOwners", method = RequestMethod.GET)
+	public List<CourseOwner> getCourseOwners(@PathVariable("courseId") Integer courseId) {
+		return null;
+	}
+
 	@RequestMapping(path = "/courses/{courseId}/assignments", method = RequestMethod.GET)
-	@ResponseBody
-	public List<AssignmentDetails> getAssignmentDetailsForCourse(@PathVariable("courseId") Integer courseId) {
-		return assignmentService.getAssignmentDetailsForCourse(courseId);
+	public List<Assignment> getAssignments(@PathVariable("courseId") Integer courseId) {
+		return null;
+	}
+	@RequestMapping(path = "/courses/{courseId}/assignments/{assignmentId}", method = RequestMethod.GET)
+	public Assignment getAssignment(@PathVariable("courseId") Integer courseId, @PathVariable("assignmentId") Integer assignmentId) {
+		return null;
+	}
+
+	@RequestMapping(path = "/courses/{courseId}/assignments/{assignmentId}/submissions", method = RequestMethod.GET)
+	public List<Submission> getSubmissions(@PathVariable("courseId") Integer courseId, @PathVariable("assignmentId") Integer assignmentId) {
+		return null;
+	}
+
+	@RequestMapping(path = "/courses/{courseId}/assignments/{assignmentId}/submissions/{submissionId}", method = RequestMethod.GET)
+	public Submission getSubmission(@PathVariable("courseId") Integer courseId, @PathVariable("assignmentId") Integer assignmentId, @PathVariable("submissionId") Integer submissionId) {
+		return null;
 	}
 }
