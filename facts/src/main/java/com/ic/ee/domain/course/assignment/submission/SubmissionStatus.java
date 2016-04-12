b6d65e0e09eff9eee2b27047063c341b985760a5
Cian Leow <cianyleow@gmail.com>
2016-04-12 12:25:01 +0100

    Expanding range of submission statuses for marking workflow.

--- facts/src/main/java/com/ic/ee/domain/course/assignment/submission/SubmissionStatus.java
+++ facts/src/main/java/com/ic/ee/domain/course/assignment/submission/SubmissionStatus.java
@@ -1,7 +1,19 @@
 package com.ic.ee.domain.course.assignment.submission;
 
 public enum SubmissionStatus {
-	CREATED,
-	VALIDATED,
-	FAILED;
+	ONTIME("This submission was received on time."),
+	LATE("This submission was received late."),
+	MARKER_ASSIGNED("This submission has been assigned to a marker."),
+	MARKING("This submission is being marked."),
+	MARKED("This submission has been marked.");
+
+	private final String message;
+
+	private SubmissionStatus(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
 }
