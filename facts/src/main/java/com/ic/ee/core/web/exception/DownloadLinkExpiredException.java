51f54c6996d91fb2b6e55aa61eda5682d620c1bc
Cian Leow <cianyleow@gmail.com>
2016-04-15 00:25:51 +0100

    Added annotations to error code correctly.

--- facts/src/main/java/com/ic/ee/core/web/exception/DownloadLinkExpiredException.java
+++ facts/src/main/java/com/ic/ee/core/web/exception/DownloadLinkExpiredException.java
@@ -1,5 +1,9 @@
 package com.ic.ee.core.web.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Link expired")
 public class DownloadLinkExpiredException extends Exception {
 
 }
--- facts/src/main/java/com/ic/ee/core/web/exception/DownloadLinkUsedException.java
+++ facts/src/main/java/com/ic/ee/core/web/exception/DownloadLinkUsedException.java
@@ -1,5 +1,9 @@
 package com.ic.ee.core.web.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Link already used")
 public class DownloadLinkUsedException extends Exception {
 
 }
