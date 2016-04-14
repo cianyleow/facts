8cb39e5eb9e6f4ca42d3c19348bc01ef905710ed
Cian Leow <cianyleow@gmail.com>
2016-04-14 21:29:11 +0100

    Made CourseOwner a super of Marker

--- facts/src/main/java/com/ic/ee/domain/user/courseowner/CourseOwner.java
+++ facts/src/main/java/com/ic/ee/domain/user/courseowner/CourseOwner.java
@@ -3,45 +3,21 @@ package com.ic.ee.domain.user.courseowner;
 import java.util.Set;
 
 import com.ic.ee.domain.course.Course;
-import com.ic.ee.domain.user.User;
+import com.ic.ee.domain.user.marker.Marker;
 
-public class CourseOwner extends User {
+public class CourseOwner extends Marker {
 
 	public CourseOwner() {
 		// TODO Auto-generated constructor stub
 	}
 
-	private String title;
-
 	private Set<Course> coursesOwned;
-
-	private Integer telephoneNumber;
 
 	public CourseOwner(String userName) {
 		super(userName);
 	}
 
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 	public Set<Course> getCoursesOwned() {
 		return coursesOwned;
-	}
-
-	public void setCoursesOwned(Set<Course> coursesOwned) {
-		this.coursesOwned = coursesOwned;
-	}
-
-	public Integer getTelephoneNumber() {
-		return telephoneNumber;
-	}
-
-	public void setTelephoneNumber(Integer telephoneNumber) {
-		this.telephoneNumber = telephoneNumber;
 	}
 }
