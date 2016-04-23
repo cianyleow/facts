c8dcbd2be6f1816a0717258aef5706ec16b779b3
Cian Leow <cianyleow@gmail.com>
2016-04-23 16:48:11 +0100

    Removed /self endpoint as info is now in token

--- facts/src/main/java/com/ic/ee/controller/SelfController.java
+++ facts/src/main/java/com/ic/ee/controller/SelfController.java
@@ -9,23 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ic.ee.domain.course.Course;
-import com.ic.ee.domain.user.User;
 import com.ic.ee.service.api.CourseService;
-import com.ic.ee.service.api.UserService;
 
 @RestController
 public class SelfController {
 
 	@Autowired
-	private UserService userService;
-
-	@Autowired
 	private CourseService courseService;
-
-	@RequestMapping(path = "/self", method = RequestMethod.GET)
-	public User getSelf(Principal user) {
-		return userService.getUser(user.getName());
-	}
 
 	@RequestMapping(path = "/self/courses", method = RequestMethod.GET)
 	public List<Course> getEnrolledCourses(Principal user) {
