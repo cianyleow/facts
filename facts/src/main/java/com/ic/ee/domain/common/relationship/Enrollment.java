a3a4c6ed7deb3f7aa333358627f6af1ed3998285
Cian Leow <cianyleow@gmail.com>
2016-04-25 11:27:20 +0100

    Expose Course and remove courseId attribute

--- facts/src/main/java/com/ic/ee/domain/common/relationship/Enrollment.java
+++ facts/src/main/java/com/ic/ee/domain/common/relationship/Enrollment.java
@@ -3,7 +3,6 @@ package com.ic.ee.domain.common.relationship;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ic.ee.domain.course.Course;
 import com.ic.ee.domain.user.student.Student;
 
@@ -43,18 +42,12 @@ public class Enrollment {
 		this.student = student;
 	}
 
-	@JsonIgnore
 	public Course getCourse() {
 		return course;
 	}
 
 	public void setCourse(Course course) {
 		this.course = course;
-	}
-
-	@JsonProperty
-	public Integer getCourseId() {
-		return course.getCourseId();
 	}
 
 	public EnrollmentLevel getEnrollmentLevel() {
