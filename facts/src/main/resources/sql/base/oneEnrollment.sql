5cd82aca669f32ecd0380a7f519a2b259e2df0d5
Cian Leow <cianyleow@gmail.com>
2016-05-02 16:40:14 +0100

    Changed the looup id for enrollments

--- facts/src/main/resources/sql/base/oneEnrollment.sql
+++ facts/src/main/resources/sql/base/oneEnrollment.sql
@@ -1,4 +1,4 @@
 SELECT enrollment.enrollmentId, enrollment.username, enrollment.courseId, enrollment.enrollmentLevel, enrollment.updateTime, 
 course.courseId, course.shortName, course.name, course.description, course.academicPeriodId 
 FROM enrollment JOIN course ON enrollment.courseId = course.courseId 
-WHERE enrollment.username = :id
+WHERE enrollment.enrollmentId = :id
