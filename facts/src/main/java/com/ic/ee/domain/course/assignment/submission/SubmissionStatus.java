70b8759b12e7bed6906489a2a0eaac7736879c94
Cian Leow <cianyleow@gmail.com>
2016-05-13 13:14:53 +0100

    Addedc CREDIT enum to submission status

--- facts/src/main/java/com/ic/ee/domain/course/assignment/submission/SubmissionStatus.java
+++ facts/src/main/java/com/ic/ee/domain/course/assignment/submission/SubmissionStatus.java
@@ -2,6 +2,7 @@ package com.ic.ee.domain.course.assignment.submission;
 
 public enum SubmissionStatus {
 	ONTIME("This submission was received on time."),
+	CREDIT("This assignment is the active assignment and will receive credit."),
 	LATE("This submission was received late."),
 	MARKER_ASSIGNED("This submission has been assigned to a marker."),
 	MARKING("This submission is being marked."),
