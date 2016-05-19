206e1ca72e5d4781e9e683d406b009a63e88de71
Cian Leow <cianyleow@gmail.com>
2016-05-19 12:47:01 +0100

    Updated sql to be correct

--- facts/src/main/resources/sql/severalCourseByMarked.sql
+++ facts/src/main/resources/sql/severalCourseByMarked.sql
@@ -1,5 +1,5 @@
 SELECT course.courseId, course.name, course.shortName, course.description, 
 academic_period.academicPeriodId, academic_period.name, academic_period.shortName, academic_period.startTime, academic_period.endTime 
 FROM course JOIN academic_period ON course.academicPeriodId = academic_period.academicPeriodId  
-JOIN marker_for ON course.courseId = owns.courseId 
-WHERE owns.username = :username
+JOIN marker_for ON course.courseId = marker_for.courseId 
+WHERE marker_for.username = :username
