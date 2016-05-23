2b4dc268e883ecf022381ed53c9b7f2c2548aa49
Cian Leow <cianyleow@gmail.com>
2016-05-23 21:50:20 +0100

    Views for @JsonView

---
+++ facts/src/main/java/com/ic/ee/domain/Views.java
@@ -0,0 +1,20 @@
+package com.ic.ee.domain;
+
+public class Views {
+
+	public static class Public {
+
+	}
+
+	public static class Student extends Public {
+
+	}
+
+	public static class Marker extends Student{
+
+	}
+
+	public static class CourseOwner extends Marker {
+
+	}
+}
