517c6186da922449c4a86a50ee11ba8edd3064ff
Cian Leow <cianyleow@gmail.com>
2016-05-25 11:49:02 +0100

    Enabled annotation security

--- facts/src/main/java/com/ic/ee/core/web/config/SecurityConfiguration.java
+++ facts/src/main/java/com/ic/ee/core/web/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import com.ic.ee.core.web.authentication.service.api.TokenUserDetailsService;
 import com.ic.ee.core.web.filter.CsrfHeaderFilter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 @Order(1)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
